781fedb736cb5ab6ded3d56deeb17693f5a42754
taogedada <837089174@qq.com>
2019-04-05 17:43:42 +0800

    first

---
+++ mytest2/src/main/java/com/syswin/rest/ConsulTest.java
@@ -0,0 +1,15 @@
+package com.syswin.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsulTest {
+
+    @RequestMapping("hello")
+    public String getResult(){
+        return "hello2";
+    }
+
+
+}
